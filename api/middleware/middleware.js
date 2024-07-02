828cd13f4947d6246bfad4bd65607b44ec2ecc7a
DerekG29 <derekgrubbs29@gmail.com>
2024-07-02 15:30:03 -0400

    built validateUserId function

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url}`
@@ -6,7 +8,20 @@ function logger(req, res, next) {
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const id = req.params.id;
+  if (typeof parseInt(id) !== 'number') {
+    res.status(422).json({ message: 'ID must be an integer' });
+  }
+  Users.getById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: 'user not found' });
+      } else {
+        req.user = user;
+        next();
+      }
+    })
+    .catch(next);
 }
 
 function validateUser(req, res, next) {
