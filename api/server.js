54e33e81d31191538efe2c5eb8f03c74e286eb1a
DerekG29 <derekgrubbs29@gmail.com>
2024-07-02 18:28:05 -0400

    cleaned up a few lines and added error handling middleware

--- api/server.js
+++ api/server.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const usersRouter = require('./users/users-router');
 
-const {
-  logger,
-  validateUserId,
-  validateUser,
-  validatePost
-} = require('./middleware/middleware');
+const { logger } = require('./middleware/middleware');
 
 const server = express();
 
@@ -14,12 +9,13 @@ server.use(express.json());
 server.use(logger);
 server.use('/api/users', usersRouter);
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
-
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use((error, req, res, next) => { // eslint-disable-line
+  console.error(error);
+  res.status(500).json({ message: 'Error performing the required operation' });
+})
 
 module.exports = server;
